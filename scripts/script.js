9f3237958c774e36241cb073e291c6426b5d2435
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-06-04 16:58:05 +0100

    Completed html

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,41 @@
-$(document).ready(function () {
+$(document).ready(() => {
   
   "use strict";
   
-  $("#search-button").click(function (event) {
-    console.log("seearch button clicked");
+  const randomURL = "https://en.wikipedia.org/wiki/Special:Random";
+  const queryURL = "https://en.wikipedia.org/w/api.php?action=query&" +
+                   "prop=extracts&exintro=true&exchars=250&generator=" +
+                   "search&format=json&gsrsearch=";
+  
+  $("#search-button").click(() => {
+    $("#search-button").attr('disabled','disabled');
+    $("#search-form-container").fadeIn().removeClass("hidden");
   });
   
-  $("#random-button").click(function (event) {
-    window.open("https://en.wikipedia.org/wiki/Special:Random");
+  $("#random-button").click(() => window.open(randomURL));
+  
+  $("#search-form").submit(e => {
+    e.preventDefault();
+    $("#search-box").blur();
+    handleSearch();
   });
+  
+  var handleSearch = () => {
+    $.ajax({
+      dataType: "jsonp",
+      url: queryURL + encodeURIComponent($("#search-box").val()),
+      success: function(data) {
+        $("#results-container").empty();
+        var pages = data.query.pages;
+        for (let page in pages) {
+          $(("<div class='search-result'>" + pages[page].extract + "</div>")).addClass("hidden").appendTo("#results-container");
+        }
+        $(".search-result").each(function (index) {
+          $(this).delay(index * 500).removeClass("hidden").fadeIn();
+        });
+        $("#results-container").removeClass("hidden");
+      }
+    });
+  };
   
 });
