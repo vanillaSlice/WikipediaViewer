8abee49483346891b34066471a2ff749c3cd3261
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-06-04 22:10:54 +0100

    Completed project

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,64 @@
-$(document).ready(() => {
+$(document).ready(function () {
   
   "use strict";
   
-  const randomURL = "https://en.wikipedia.org/wiki/Special:Random";
-  const queryURL = "https://en.wikipedia.org/w/api.php?action=query&" +
-                   "prop=extracts&exintro=true&exchars=250&generator=" +
-                   "search&format=json&gsrsearch=";
+  var randomURL = "https://en.wikipedia.org/wiki/Special:Random",
+    queryURL = "https://en.wikipedia.org/w/api.php?action=query" +
+      "&prop=extracts&exintro=true&exsentences=1&explaintext=true" +
+      "&generator=search&format=json&gsrsearch=",
+    wikipediaURL = "https://en.wikipedia.org/wiki/",
+    searchButton = $("#search-button"),
+    searchBox = $("#search-box"),
+    resultsContainer = $("#results-container");
   
-  $("#search-button").click(() => {
-    $("#search-button").attr("disabled", true);
+  searchButton.click(function () {
+    searchButton.attr("disabled", true);
     $("#search-form-container").addClass("animated fadeIn").removeClass("hidden");
   });
   
-  $("#random-button").click(() => window.open(randomURL));
+  $("#random-button").click(function () {
+    window.open(randomURL);
+  });
   
-  $("#search-form").submit(e => {
-    e.preventDefault();
-    $("#search-box").blur();
+  $("#search-form").submit(function (event) {
+    event.preventDefault();
+    searchBox.blur();
     handleSearch();
   });
   
-  var handleSearch = () => {
+  function handleSearch() {
     $.ajax({
       dataType: "jsonp",
-      url: queryURL + encodeURIComponent($("#search-box").val()),
-      success: function(data) {
-        var resultsContainer = $("#results-container");
-        resultsContainer.empty().removeClass("hidden");
-        var results = $("<div>", {"id": "results", "class": "animated slideInUp"});
-        results.appendTo(resultsContainer);
-        var pages = data.query.pages;
-        for (let page in pages) {
-          $("<div class='search-result' tabindex='0'><h2>" + pages[page].title + "</h2><p>" + pages[page].extract + "</p></div>").appendTo(results);
+      url: queryURL + encodeURIComponent(searchBox.val()),
+      success: handleSuccess,
+      error: handleError
+    });
+  }
+  
+  function handleSuccess(data) {
+    resultsContainer.empty().removeClass("hidden");
+    
+    var results = $("<div>", {"id": "results", "class": "animated slideInUp"});
+    results.appendTo(resultsContainer);
+    
+    if (data.query === undefined || data.query.pages === undefined) {
+      results.append("No results returned");
+    } else {
+      var pages = data.query.pages;
+      for (var key in pages) {
+        if (pages.hasOwnProperty(key)) {
+          var page = pages[key],
+            href = wikipediaURL + page.title.replace(" ", "_");
+            $("<a class='search-result' href='" + href + "' target='_blank'>" +
+            "<div><h2>" + page.title + "</h2><p>" + page.extract + "</p></div>" +
+            "</a>").appendTo(results);
         }
       }
-    });
-  };
+    }
+  }
+  
+  function handleError(error) {
+    console.error(error);
+  }
   
 });
