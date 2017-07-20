b0c2cfdb6a0ab0859f9439b271a21449fe4a2963
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-07-20 19:14:57 +0100

    Updated to use github pages

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # Wikipedia Viewer
 Intermediate Front End Development Project for FreeCodeCamp.
 
-This page is intended to be functionally similar to [this example page](https://codepen.io/freeCodeCamp/full/wGqEga).
-
-A codepen of my page can be found [here](https://codepen.io/vanillaSlice/full/vZBpWE/).
-
-## Screenshot
-![screenshot](./screenshot.png)
+A working version of this page can be found [here](https://vanillaslice.github.io/WikipediaViewer/).
--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
-  
+
   "use strict";
-  
+
   var randomURL = "https://en.wikipedia.org/wiki/Special:Random",
     queryURL = "https://en.wikipedia.org/w/api.php?action=query" +
-      "&prop=extracts&exintro=true&exsentences=1&explaintext=true" +
-      "&generator=search&format=json&gsrsearch=",
+    "&prop=extracts&exintro=true&exsentences=1&explaintext=true" +
+    "&generator=search&format=json&gsrsearch=",
     wikipediaURL = "https://en.wikipedia.org/wiki/",
     searchButton = $("#search-button"),
     searchBox = $("#search-box"),
     resultsContainer = $("#results-container");
-  
+
   searchButton.click(function () {
     searchButton.attr("disabled", true);
     $("#search-form-container").addClass("animated fadeIn").removeClass("hidden");
   });
-  
+
   $("#random-button").click(function () {
     window.open(randomURL);
   });
-  
+
   $("#search-form").submit(function (event) {
     event.preventDefault();
     searchBox.blur();
     handleSearch();
   });
-  
+
   function handleSearch() {
     $.ajax({
       dataType: "jsonp",
@@ -34,13 +34,16 @@ $(document).ready(function () {
       error: handleError
     });
   }
-  
+
   function handleSuccess(data) {
     resultsContainer.empty().removeClass("hidden");
-    
-    var results = $("<div>", {"id": "results", "class": "animated slideInUp"});
+
+    var results = $("<div>", {
+      "id": "results",
+      "class": "animated slideInUp"
+    });
     results.appendTo(resultsContainer);
-    
+
     if (data.query === undefined || data.query.pages === undefined) {
       results.append("No results returned");
     } else {
@@ -49,16 +52,16 @@ $(document).ready(function () {
         if (pages.hasOwnProperty(key)) {
           var page = pages[key],
             href = wikipediaURL + page.title.replace(" ", "_");
-            $("<a class='search-result' href='" + href + "' target='_blank'>" +
+          $("<a class='search-result' href='" + href + "' target='_blank'>" +
             "<div><h2>" + page.title + "</h2><p>" + page.extract + "</p></div>" +
             "</a>").appendTo(results);
         }
       }
     }
   }
-  
+
   function handleError(error) {
     console.error(error);
   }
-  
+
 });
