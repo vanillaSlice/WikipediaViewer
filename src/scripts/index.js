5ae492aeed63a15f00b4d8cb1e4ce9bef7ae6e95
Mike Lowe <mikelowedev@gmail.com>
2019-01-27 19:26:27 +0000

    Tidy up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,51 +1,51 @@
 /*
  * URLs
  */
-const wikipediaURL = 'https://en.wikipedia.org/wiki/';
-const queryURL = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=true&exsentences=1&explaintext=true&generator=search&format=json&gsrsearch=';
-const randomURL = 'https://en.wikipedia.org/wiki/Special:Random';
+
+const wikipediaUrl = 'https://en.wikipedia.org/wiki/';
+const queryUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=true&exsentences=1&explaintext=true&generator=search&format=json&gsrsearch=';
+const randomUrl = 'https://en.wikipedia.org/wiki/Special:Random';
 
 /*
- * DOM elements
+ * DOM Elements
  */
-const searchBtnElement = $('.wiki-viewer__search-btn');
-const randomBtnElement = $('.wiki-viewer__random-btn');
-const searchFormContainerElement = $('.wiki-viewer__search-form-container');
-const searchFormElement = $('.wiki-viewer__search-form');
-const searchInputElement = $('.wiki-viewer__search-input');
-const resultsContainerElement = $('.wiki-viewer__results-container');
 
-// show search form on search button click
-searchBtnElement.click(() => {
+const searchBtnElement = $('.js-search-btn');
+const randomBtnElement = $('.js-random-btn');
+const searchFormContainerElement = $('.js-search-form-container');
+const searchFormElement = $('.js-search-form');
+const searchInputElement = $('.js-search-input');
+const resultsContainerElement = $('.js-results-container');
+
+/*
+ * Functions
+ */
+
+function handleSearchBtnClick() {
   searchBtnElement.attr('disabled', true);
   searchFormContainerElement.addClass('animated fadeIn').removeClass('hidden');
-});
+}
 
-// open random wiki entry on random button click
-randomBtnElement.click(() => {
-  window.open(randomURL);
-});
+function handleRandomBtnClick() {
+  window.open(randomUrl);
+}
 
-function handleSuccess(data) {
-  // make sure we unhide the results container
+function handleSearchSuccess(data) {
   resultsContainerElement.empty().removeClass('hidden');
 
-  // create div to append each search result to
-  const results = $('<div class="animated slideInUp wiki-viewer__search-results">');
+  const results = $('<div class="wiki-viewer__search-results animated slideInUp">');
 
   results.appendTo(resultsContainerElement);
 
-  // no results could be found
   if (!data.query || !data.query.pages) {
     results.append('No results returned');
     return;
   }
 
-  // append each result to results
   const { pages } = data.query;
   Object.keys(pages).forEach((key) => {
     const page = pages[key];
-    const href = wikipediaURL + page.title.replace(' ', '_');
+    const href = wikipediaUrl + page.title.replace(' ', '_');
 
     results.append(
       `<a class="wiki-viewer__search-result" href="${href}" target="_blank">`
@@ -58,22 +58,29 @@ function handleSuccess(data) {
   });
 }
 
-function handleError(error) {
+function handleSearchError(error) {
   console.error(error);
 }
 
 function handleSearch() {
   $.ajax({
     dataType: 'jsonp',
-    url: queryURL + encodeURIComponent(searchInputElement.val()),
-    success: handleSuccess,
-    error: handleError,
+    url: queryUrl + encodeURIComponent(searchInputElement.val()),
+    success: handleSearchSuccess,
+    error: handleSearchError,
   });
 }
 
-// perform search on search form submit
-searchFormElement.submit((event) => {
-  event.preventDefault();
+function handleSearchFormSubmit(e) {
+  e.preventDefault();
   searchInputElement.blur();
   handleSearch();
-});
+}
+
+/*
+ * Initialise
+ */
+
+searchBtnElement.click(handleSearchBtnClick);
+randomBtnElement.click(handleRandomBtnClick);
+searchFormElement.submit(handleSearchFormSubmit);
